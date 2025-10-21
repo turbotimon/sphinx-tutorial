c544b0524f79253efb81ec15bbc665622641d98d
Timon Erhart <57718207+turbotimon@users.noreply.github.com>
2025-10-21 10:43:34 +0000

    add extension

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,9 @@ release = 'v0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
